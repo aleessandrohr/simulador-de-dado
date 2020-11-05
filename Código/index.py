4f492f3f7a9f93798ac2eaa6abab77f8f76a9a05
Alessandro Ramos <toprime13@gmail.com>
2020-11-05 07:12:57 -0300

    Atualizado código

--- Código/index.py
+++ Código/index.py
@@ -7,7 +7,7 @@ while True:
     op = opção()
     if op == 1:
         opc = leiaint('Quantos dados deseja gerar? ')
-        linha('Gerando Dados', cor='\033[33m')
+        linha('Gerando Dados', cor='\033[33m', show=False)
         for c in range(opc):
             print(f'\033[30m{c+1}º Dado: {randint(0, 6)}\033[m')
             sleep(0.5)
