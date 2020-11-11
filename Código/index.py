14f45ecff464b1ec6575d9f08a5c6001910f27d7
Alessandro Ramos <toprime13@gmail.com>
2020-11-10 22:48:14 -0300

    Adicionado interface gráfica

--- Código/index.py
+++ Código/index.py
@@ -1,17 +1,6 @@
-from random import randint
-from time import sleep
 from interface import *
 
-while True:
-    linha('Simulador de Dado', cor='\033[33m')
-    op = opção()
-    if op == 1:
-        opc = leiaint('Quantos dados deseja gerar? ')
-        linha('Gerando Dados', cor='\033[33m', show=False)
-        for c in range(opc):
-            print(f'\033[30m{c+1}º Dado: {randint(0, 6)}\033[m')
-            sleep(0.5)
-    if op == 2:
-        linha('FINALIZANDO PROGAMA', cor='\033[31m', show=False)
-        sleep(1)
-        break
+
+tela.Iniciar()
+
+
--- Código/interface.py
+++ Código/interface.py
@@ -1,33 +1,38 @@
-def leiaint(número):
-    while True:
-        try:
-            número = int(input(f'{número}'))
-        except:
-            print(f'\033[31mPor favor digite um número inteiro.\033[m')
-        else:
-            return número
+import PySimpleGUI as sg
+from time import sleep
+from random import randint
+
+class Interface:
+    def __init__(self):
+        sg.change_look_and_feel('Default1')
+        # Layout
+        layout = [
+            [sg.Button('Gerar dados')],
+            [sg.Input(size=(30,0), key='dados')],
+            [sg.Output(size=(30,10))]
+        ]
+        # Janela
+        self.janela = sg.Window('Simulador de Dados').layout(layout)
 
 
-def linha(msg='', cor='\033[m', fim='\033[m', show=True):
-    if show == True:
-        print(f'{cor}-' * 42)
-        print(msg.center(42))
-        print(f'{cor}-{fim}' * 42)
-        print('\033[34m1 - Gerar dado')
-        print('2 - Sair do progama\033[m')
-        print(f'{cor}-{fim}' * 42)
-    else:
-        print(f'{cor}-' * 42)
-        print(msg.center(42))
-        print(f'{cor}-{fim}' * 42)
+    def Iniciar(self):
+        while True:
+            # Extrair os dados da tela
+            self.button, self.values = self.janela.Read()
+            try:
+                dado = int(self.values['dados'])
+            except:
+                pass
+            else:
+                if dado != 0:
+                    c = 1
+                    print('-' * 52)
+                    while True:
+                        print(f'{c}º Dado: {randint(0, 6)}')
+                        sleep(0.5)
+                        if c == dado:
+                            break
+                        c += 1
+                    print('-' * 52)
+tela = Interface()
 
-
-def opção():
-    while True:
-        opção = leiaint('Sua opção: ')
-        if opção == 1:
-            return opção
-        elif opção == 2:
-            return opção
-        else:
-            print('\033[31mDigite uma opção válida!\033[m')
